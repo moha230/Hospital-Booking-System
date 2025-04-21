77dea39ba40bf230d5e966759e6c8d07fb64a3e8
AF7626 <af7626@student.jamk.fi>
2025-04-22 01:13:21 +0300

    login and create account state is functional

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Hello this is the Login page</div>;
+  //Track whether the user is signing up or logging in
+  //Possible values 'Sign up' or 'Login'
+  const [state, setState] = useState("Login");
+
+  //Stores the users full name input (only used in Sign Up mode)
+  const [name, setName] = useState("");
+
+  //Stores the users emails input (used in both Login and Sign up)
+  const [email, setEmail] = useState("");
+
+  //Stores user password input (used in both Login and Sign Up)
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form>
+      <div>
+        <p>{state === 'Sign Up' ? "Create Account":"Login"}</p>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
