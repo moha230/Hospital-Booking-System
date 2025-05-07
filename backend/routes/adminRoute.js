d48dc83b704f5719e492aa691dd2725e75b7cd8c
AF7626 <af7626@student.jamk.fi>
2025-05-07 14:23:48 +0300

    feat(admin): implement /add-doctor route with image upload using multer

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,21 +1,19 @@
 import express from 'express';
-import { addUser, loginAdmin ,allUsers} from '../controllers/adminController.js';
+import {  addDoctor,} from '../controllers/adminController.js';
+
+import upload from "../middlewares/multer.js";
+
 import authAdmin from '../middlewares/authAdmin.js';
 
 // Create a new router instance for handling admin-related routes
 const adminRouter = express.Router();
 
 
-// This handles POST requests to the endpoint: http://localhost:3000/api/v1/admin/add-user
-// Route to login the admin
-adminRouter.post('/login', loginAdmin);
 
-// Route to add a new user
-adminRouter.post('/add-user', authAdmin, addUser);
+// Route to add a new doctor
+adminRouter.post('/add-doctor', addDoctor,upload.single('image'));
 
-//Route to get all user 
 
-adminRouter.get("/all-users", authAdmin, allUsers)
 
 
 
