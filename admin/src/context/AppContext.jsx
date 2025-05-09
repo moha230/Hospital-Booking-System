713e476ff534b08afc2a8c123436a711234835b1
AF7626 <af7626@student.jamk.fi>
2025-05-09 03:02:16 +0300

    intial config for admin panel

---
+++ admin/src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import { doctors } from "@/assets/assets";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+
+  const currencySymbols = "€";  //
+
+  const value = {
+    doctors,
+    currencySymbols
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react'
-import path from 'path'; 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { 
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
-    },
-    server:{port:5173}
 })
