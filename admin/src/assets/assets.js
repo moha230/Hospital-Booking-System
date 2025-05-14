9eda7683c4fff4ead1958045acb58dc1d035a7c8
AF7626 <af7626@student.jamk.fi>
2025-05-14 03:58:53 +0300

    chore: add static assets for admin panel

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,36 @@
+import add_icon from './add_icon.svg'
+import admin_logo from './admin_logo.svg'
+import appointment_icon from './appointment_icon.svg'
+import appointments_icon from './appointments_icon.svg'
+import cancel_icon from './cancel_icon.svg'
+import checked_icon from './checked_icon.svg'
+import checklist from './checklist.svg'
+import doctor_icon from './doctor_icon.svg'
+import home_icon from './home_icon.svg'
+import money_bag from './money-bag.svg'
+import upload_area from './upload_area.svg'
+import patient_icon from './patient_icon.svg'
+import patients_icon from './patients_icon.svg'
+import people_icon from './people_icon.svg'
+
+
+
+
+export const assets = {
+    add_icon,
+    admin_logo,
+    appointment_icon,
+    appointments_icon,
+    cancel_icon,
+    checked_icon,
+    checklist,
+    doctor_icon,
+    home_icon,
+    money_bag ,
+    patient_icon,
+    patients_icon,
+    people_icon,
+    upload_area
+   
+   
+}
