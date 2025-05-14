6dc3d15eb33f84d60a67ae7aedfdc958b2174746
AF7626 <af7626@student.jamk.fi>
2025-05-14 04:15:46 +0300

    feat: integrate Login into App

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { Login } from './pages/Login'
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Login/>
+    </div>
   )
 }
 
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Login() {
+const Login = () => {
   return (
     <div>Login</div>
   )
 }
+
+
+export {Login}
