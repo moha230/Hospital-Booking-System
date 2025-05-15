c7690522392d8c721b95dd979ed9e3ce95ad7142
AF7626 <af7626@student.jamk.fi>
2025-05-15 11:12:42 +0300

    feat: added <ToastContainer /> to App for toast notifications

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Login } from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   return (
     <div>
       <Login/>
+      <ToastContainer/>
     </div>
   )
 }
