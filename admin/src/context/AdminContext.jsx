a9c6da927af56e745a5049a09f499a90b8ae7d07
AF7626 <af7626@student.jamk.fi>
2025-05-15 10:00:02 +0300

    feat: create AdminContext for managing admin token and backend URL

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,20 +1,33 @@
-import { createContext } from "react";
+import { createContext, useState } from "react"; //importing necessary hooks and functions from react 
 
 
-export const AdminContext= createContext()
+// Create a new context object called AdminContext
+export const AdminContext = createContext();
+
 
 
 const AdminContextProvider = (props) => {
+
+//admin token and settoken with intitial string
+const [adminToken, setAdmintoken] = useState("");
+
+
+// Get the backend URL from the environment variables
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+// The `value` object will made avialable for all childe components through context.
   const value = {
+    adminToken,
+    setAdmintoken,
+    backendUrl 
+  };
 
-  }
-
+  // This makes the `value` available to all child components wrapped by provider.
   return (
     <AdminContext.Provider value={value}>
-     {props.children}
+      {props.children}
     </AdminContext.Provider>
+  );
+};
 
-  )
-}
-
-export default AdminContextProvider
+export default AdminContextProvider;
