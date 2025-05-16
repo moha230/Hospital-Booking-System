65f76bd0d5dcbb0f5376ef8c911899d6b1d9b10a
AF7626 <af7626@student.jamk.fi>
2025-05-16 09:59:23 +0300

    feat: add conditional routing and layout for admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,18 @@ import { ToastContainer, toast } from 'react-toastify';
 import {AdminContext} from './context/AdminContext'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import {Route,Routes} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import AllAppointments from './pages/AllAppointments';
+import AddDoctor from './pages/AddDoctor';
+import DoctorsList from './pages/DoctorsList';
+import DoctorDashboard from './pages/DoctorDashboard';
+import DoctorAppointments from './pages/DoctorAppointments';
+import DoctorProfile from './pages/DoctorProfile';
+
+
+
+
 
 function App() {
 
@@ -17,6 +29,16 @@ const {adminToken} = useContext(AdminContext)
       <Navbar/>
       <div className='flex items-end'>
         <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
+        </Routes>
       </div>
     </div>
   ) : (
