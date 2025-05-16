2221abd84b67815472598aeef8500e5eaccd7815
AF7626 <af7626@student.jamk.fi>
2025-05-16 10:45:53 +0300

    feat&fix: add admin sidebar with navigation links and icons and renamed Allpointments to Allappointments

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,15 @@
 import React, { useContext } from 'react'
-import { Login } from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify';
-import {AdminContext} from './context/AdminContext'
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
 import {Route,Routes} from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import AllAppointments from './pages/AllAppointments';
-import AddDoctor from './pages/AddDoctor';
-import DoctorsList from './pages/DoctorsList';
-import DoctorDashboard from './pages/DoctorDashboard';
-import DoctorAppointments from './pages/DoctorAppointments';
-import DoctorProfile from './pages/DoctorProfile';
+import { ToastContainer, toast } from 'react-toastify';
+import {AdminContext} from '@/context/AdminContext'
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import { Login } from '@/pages/Login'
+import Dashboard from '@/pages/Admin/Dashboard';
+import AllAppointments from '@/pages/Admin/AllAppointments';
+import AddDoctor from '@/pages/Admin/AddDoctor';
+import DoctorsList from '@/pages/Admin/DoctorsList';
+
 
 
 
@@ -27,18 +25,17 @@ const {adminToken} = useContext(AdminContext)
      
       <ToastContainer/>
       <Navbar/>
-      <div className='flex items-end'>
+      <div className='flex flex-row-reverse min-h-screen'>
         <Sidebar/>
+        <div className='flex-1 p-4'>
         <Routes>
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorsList />} />
-          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
-          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
-          <Route path='/doctor-profile' element={<DoctorProfile />} />
         </Routes>
+        </div>
       </div>
     </div>
   ) : (
--- admin/src/components/sidebar.jsx
+++ admin/src/components/sidebar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AdminContext } from "@/context/AdminContext";
+import { NavLink } from "react-router-dom";
+import { assets } from "@/assets/assets";
 
-function sidebar() {
+const sidebar = () => {
+  const { adminToken } = useContext(AdminContext);
+
   return (
-    <div>sidebar</div>
-  )
-}
+    <div>
+      {adminToken && (
+        <ul>
+          <NavLink to={"/admin-dashboard"}>
+            <img src={assets.home_icon} />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink to={"/all-appointments"}>
+            <img src={assets.appointment_icon} />
+            <p>Appointment</p>
+          </NavLink>
+          <NavLink to={"/add-doctor"}>
+            <img src={assets.add_icon} />
+            <p>Add Doctor</p>
+          </NavLink>
+          <NavLink to={"/doctor-list"}>
+            <img src={assets.people_icon} />
+            <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default sidebar
+export default sidebar;
