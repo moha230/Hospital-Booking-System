91911e33fa4b964e2d788af78aff003652c7796b
AF7626 <af7626@student.jamk.fi>
2025-05-24 21:30:04 +0300

    feat:add login api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@
 import validator from 'validator';
 import bcrypt from "bcrypt";
 import { createUser } from "../services/userService.js";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 // Controller function to register a new user
 const userRegistration = async (req, res) => {
@@ -39,6 +41,29 @@ const userRegistration = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email })
+    if (!user) {
+      res.json({ success: false, message: 'User does not exit please try registering' });
+    }
 
+    const userMatch = await bcrypt.compare(password, user.password)
 
-export { userRegistration };
+    if (userMatch) {
+      const userToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+      res.json({ success: true, token:userToken })
+    } else
+    {
+      res.json({ success: false, message:"Invalid credentials" })
+    }
+  
+
+  } catch (error) {
+  console.error(error);
+  res.status(500).json({ success: false, message: error.message });
+}
+}
+
+export { userRegistration , loginUser};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userRegistration } from '../controllers/userController.js';
+import { userRegistration,loginUser} from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
@@ -7,7 +7,8 @@ const userRouter = express.Router();
 // route to register new  user 
 userRouter.post("/register", userRegistration)
 
-
+//route for user to login 
+userRouter.post('/login',loginUser)
 
 
 export default userRouter
