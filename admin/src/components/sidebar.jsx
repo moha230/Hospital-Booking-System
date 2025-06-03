303dccdbe21f31aac3921577f2d5f115df907108
AF7626 <af7626@student.jamk.fi>
2025-06-04 01:04:18 +0300

    feat: add sidebar navigation for doctor dashboard, appointments, and profile

--- admin/src/components/sidebar.jsx
+++ admin/src/components/sidebar.jsx
@@ -2,9 +2,10 @@ import React, { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
-
+import { DoctorContext } from "../context/DoctorContext";
 const Sidebar = () => {
   const { adminToken } = useContext(AdminContext);
+  const { doctorToken } = useContext(DoctorContext);
 
   return (
     <div className="min-h-screen bg-white border-r">
@@ -53,6 +54,43 @@ const Sidebar = () => {
           >
             <img src={assets.people_icon} />
             <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+      {doctorToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            to={"/doctor-dashboard"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+          >
+            <img className="min-w-5" src={assets.home_icon} alt="" />
+            <p className="hidden md:block">Dashboard</p>
+          </NavLink>
+          <NavLink
+            to={"/doctor-appointments"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+          >
+            <img className="min-w-5" src={assets.appointment_icon} alt="" />
+            <p className="hidden md:block">Appointments</p>
+          </NavLink>
+          <NavLink
+            to={"/doctor-profile"}
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+          >
+            <img className="min-w-5" src={assets.people_icon} alt="" />
+            <p className="hidden md:block">Profile</p>
           </NavLink>
         </ul>
       )}
