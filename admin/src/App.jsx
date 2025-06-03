0c516b0095246450d6b37918348587fc43d53f4f
AF7626 <af7626@student.jamk.fi>
2025-06-03 21:34:53 +0300

    feat:Add dynamic title updates based on user and current route admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
 import { Login } from "./pages/Login";
@@ -11,8 +11,32 @@ import DoctorRegistration from "./pages/Admin/DoctorRegistration";
 import DoctorsList from "./pages/Admin/DoctorsList";
 
 const App = () => {
-  //test
-  const { adminToken } = useContext(AdminContext);
+  const { adminToken, adminInfo } = useContext(AdminContext); // 
+  const location = useLocation();
+
+  useEffect(() => {
+    const getTitle = (pathname) => {
+      if (!adminToken) return "Admin Login";
+
+      if (pathname === "/admin-dashboard")
+        return adminInfo?.name
+          ? `Dashboard - Dr. ${adminInfo.name} | Unix-doctors`
+          : "Admin Dashboard";
+
+      if (pathname === "/all-appointments")
+        return "Appointments";
+
+      if (pathname === "/doctor-registration")
+        return "Doctor Registration";
+
+      if (pathname === "/list-all-doctors")
+        return "Doctors List";
+
+      return "Unix-doctors Admin";
+    };
+
+    document.title = getTitle(location.pathname);
+  }, [location, adminToken, adminInfo]);
 
   return adminToken ? (
     <div className="bg-background">
@@ -25,10 +49,7 @@ const App = () => {
             <Route path="/" element={<></>} />
             <Route path="/admin-dashboard" element={<Dashboard />} />
             <Route path="/all-appointments" element={<AllAppointments />} />
-            <Route
-              path="/doctor-registration"
-              element={<DoctorRegistration />}
-            />
+            <Route path="/doctor-registration" element={<DoctorRegistration />} />
             <Route path="/list-all-doctors" element={<DoctorsList />} />
           </Routes>
         </div>
